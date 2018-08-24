61db3d9e31fd0767f4db4d19d6a00b4fce785ca2
misternode <misterynode@gmail.com>
2018-08-24 08:49:34 -0500

    Add whitespace

--- mod_php/.credentials.php
+++ mod_php/.credentials.php
@@ -1,10 +1,12 @@
 <?php
+
 $rpc_credentials = array(
     'user' => '',
     'password' => '',
     'host' => 'localhost',
     'port' => '17710',
 );
+
 $db_credentials = array(
     'user' => 'mogwai',
     'password' => '',
